b45b52f30bae411502850d41abac8732855fa91a
carlos <huaixian.huang@gmail.com>
2012-05-24 22:41:23 +0800

    base files

---
+++ gen/com/kyomen/alarm/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.kyomen.alarm;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/kyomen/alarm/Kyomen_alarmActivity.java
@@ -0,0 +1,13 @@
+package com.kyomen.alarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Kyomen_alarmActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
