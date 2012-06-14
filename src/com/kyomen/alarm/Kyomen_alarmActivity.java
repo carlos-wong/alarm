d990d3177ad0cd673ee046c109bbc9804ffa3db6
carlos <huaixian.huang@gmail.com>
2012-06-14 21:01:07 +0800

    add a button to test use intent to start other app to save data

--- src/com/kyomen/alarm/Kyomen_alarmActivity.java
+++ src/com/kyomen/alarm/Kyomen_alarmActivity.java
@@ -1,15 +1,26 @@
 package com.kyomen.alarm;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.apache.http.util.EncodingUtils;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Vibrator;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 //TODO 当程序退出的时候定时器可能没有退出，所以需要程序退出的时候检查定时器。重复退出是否会有问题
 
@@ -18,11 +29,12 @@ public class Kyomen_alarmActivity extends Activity {
 	public static final int check_edit = 1;
 	public static final int enough_length = 16;
 	private TextView data_add;
+	private Button button_share;
 	private int textlength = 0;
 	private Timer timer;
 	Vibrator vibrator;// = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 	final long[] vibrate_pattern = {800, 50, 400, 30}; // OFF/ON/OFF/ON...  
-
+	MediaPlayer mplayer;
 
 	private Handler myHandler = new Handler(){
 		public void handleMessage(Message msg)
@@ -38,13 +50,15 @@ public class Kyomen_alarmActivity extends Activity {
 					//TODO 储存的方式不应该每次设置应该有一个默认的值。给用户一个反应的时间。当然第一次设置之前这个提示应该是一直存在的。
 					Log.v("", "YEAH YOU FINISH");
 					timer.cancel();
-					vibrator.cancel();
+					//vibrator.cancel();
+					//TODO try to send the message to the other app
+					
 				}
 				else
 				{
 					//TODO do something to alarm
 					Log.v("", "ARE YOU SLEEP?");
-					vibrator.vibrate(vibrate_pattern, 2);//TODO you must make sure this work well
+					//vibrator.vibrate(vibrate_pattern, 2);//TODO you must make sure this work well
 				}
 				break;
 			}
@@ -57,15 +71,42 @@ public class Kyomen_alarmActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         data_add = (TextView)findViewById(R.id.data_input);
+        button_share = (Button)findViewById(R.id.button_share);
         timer = new Timer();
-        try{
-        	timer.scheduleAtFixedRate(new myTask(), 1, 5000);
-        }
-        catch (Exception e) {
-        	e.printStackTrace();
-		}
-        vibrator =  (Vibrator) getSystemService(VIBRATOR_SERVICE);
-    }
+//        try{
+//        	timer.scheduleAtFixedRate(new myTask(), 1, 5000);
+//        }
+//        catch (Exception e) {
+//        	e.printStackTrace();
+//		}
+        //vibrator =  (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        mplayer = MediaPlayer.create(this, R.raw.ring);
+          	mplayer.start();
+          	
+          	
+		OnClickListener listener = new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Log.v("MyTag", "the onclick button id is "+v.getId());
+
+				if (v.getId() == R.id.button_share) {
+					Log.v("MyTag",
+							"run the code to handler click button share");
+					Intent intent = new Intent(Intent.ACTION_SEND);
+					intent.setType("text/plain");
+					intent.putExtra(Intent.EXTRA_SUBJECT, "分享");
+					intent.putExtra(Intent.EXTRA_TEXT,
+							"I would like to share this with you...");
+					startActivity(Intent.createChooser(intent, getTitle()));
+				}
+			}
+		};
+		button_share.setOnClickListener(listener);
+	}
+    
+    
+    
     private class myTask extends TimerTask{
     	@Override
     	public void run(){
