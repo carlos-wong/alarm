b7dd9610ceafce16952b8accf28da48daee08164
carlos <huaixian.huang@gmail.com>
2012-06-14 22:55:08 +0800

    add alaram alert class

---
+++ src/com/kyomen/alarm/Alarmalert.java
@@ -0,0 +1,29 @@
+package com.kyomen.alarm;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class Alarmalert extends Activity {
+	  @Override
+	  protected void onCreate(Bundle savedInstanceState) 
+	  {
+	    super.onCreate(savedInstanceState);
+	    /* 跳出的闹铃警示  */
+	    new AlertDialog.Builder(this)
+	        .setTitle("闹钟响了!!")
+	        .setMessage("赶快起床吧!!!")
+	        .setPositiveButton("关掉他",
+	         new DialogInterface.OnClickListener()
+	        {
+	          public void onClick(DialogInterface dialog, int whichButton)
+	          {
+	            /* 关闭Activity */
+	            finish();
+	          }
+	        })
+	        .show();
+	  } 
+}
