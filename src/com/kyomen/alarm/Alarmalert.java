82f6b6d8e321eddc7f6906c029dcbd77b364d427
carlos <huaixian.huang@gmail.com>
2012-07-01 18:49:05 +0800

    when start alarm activity continute check user input of textview

--- src/com/kyomen/alarm/Alarmalert.java
+++ src/com/kyomen/alarm/Alarmalert.java
@@ -6,24 +6,45 @@ import java.util.TimerTask;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.widget.TextView;
 
 public class Alarmalert extends Activity {
 	
 	public static final int check_edit = 1;
 	private Timer timer;
-
+	private TextView data_add;
+	int last_input_len = 0;
+	int input_len = 0;
+	public static final int total_length = 128;
+	int dialog_showed = 0;
 	
 	private Handler myHandler = new Handler(){
 		public void handleMessage(Message msg)
 		{
 			switch(msg.what){
 			case check_edit:
-				Log.v("","timer to check edit");
+				input_len = data_add.getText().length();
+				Log.v("","timer to check edit text leng is: "+input_len+" last input length is "+last_input_len);
+				if(input_len <= last_input_len)
+				{
+					Log.v("","you must fall alseep");
+					if(dialog_showed == 0)
+					{
+						showDialog(Alarmalert.this);
+						dialog_showed = 1;
+					}	
+				}
+				if(input_len >= total_length)
+				{
+					Log.v("","the worker finish work");
+				}
+				last_input_len = input_len;
 			}
 		};
 	};
@@ -34,7 +55,7 @@ public class Alarmalert extends Activity {
     		Message message = new Message();
     		message.what = check_edit;
     		myHandler.sendMessage(message);
-    		Log.v("","timer task run");
+    		//Log.v("","timer task run");
     	}
 	}
     
@@ -43,7 +64,11 @@ public class Alarmalert extends Activity {
 	  protected void onCreate(Bundle savedInstanceState) 
 	  {
 	    super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarmalert);
 	    Log.v("alarmalert","the activity to recive alarm data");
+	    
+	    data_add  = (TextView)findViewById(R.id.dailyinput);
+	    
 		timer = new Timer();
 
 		try {
@@ -67,4 +92,20 @@ public class Alarmalert extends Activity {
 //	        })
 //	        .show();
 	  } 
+	  
+	  
+	  private void showDialog(Context context) {  
+	        AlertDialog.Builder builder = new AlertDialog.Builder(context);  
+	        builder.setTitle("Title");  
+	        builder.setMessage("Message");  
+	        builder.setPositiveButton("OK",  
+	                new DialogInterface.OnClickListener() {  
+	                    public void onClick(DialogInterface dialog, int whichButton) {  
+	                        //finish(); 
+	                        dialog_showed = 0;
+	                        Log.v("","some one click the ok button");
+	                    }  
+	                });   
+	        builder.show();  
+	    } 
 }
